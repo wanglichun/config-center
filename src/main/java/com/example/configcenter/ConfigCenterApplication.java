621b0902d88ecadda2e1b5bd48b4626f6d56a604
wanglichun <lichun.wang@shopee.com>
2025-07-01 11:32:49 +0800

    fix

--- src/main/java/com/example/configcenter/ConfigCenterApplication.java
+++ src/main/java/com/example/configcenter/ConfigCenterApplication.java
@@ -29,19 +29,5 @@ public class ConfigCenterApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigCenterApplication.class, args);
-        System.out.println("========================================");
-        System.out.println("  配置中心启动成功！");
-        System.out.println("  访问地址: http://localhost:8080/config-center");
-        System.out.println("  Swagger文档: http://localhost:8080/config-center/swagger-ui.html");
-        System.out.println("  监控面板: http://localhost:8080/config-center/actuator");
-        System.out.println("========================================");
-
-        Map<String, Map<?, ?>> allEnums = EnumUtils.getAllEnumMaps(
-                EnvironmentEnum.class,
-                ConfigStatusEnum.class
-        );
-
-        // 打印结果
-        System.out.println(allEnums);
     }
 } 
